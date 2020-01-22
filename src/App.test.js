cbd2b6f44f51ba82a4abbd097f49290a5be46d47
Gillian <gillian@yeomans.org.uk>
2020-01-22 12:07:19 +0000

    Add initial tests
    Co-authored-by: Renata <renatajarmova@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+test("landing page loads app title", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Signpost/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("landing button triggers icons page", () => {
+  const { getByText } = render(<App />);
+  const buttonNode = getByText("Get started");
+  fireEvent.click(buttonNode);
+  getByText(/Choose a service/i);
 });
