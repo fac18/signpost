61e89134265ec8b9ff9b13d3c15db035bb2070e3
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-01-22 10:55:22 +0000

    add get data function

---
+++ src/utils/getData.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+// API call
+
+const checkResponse = response => {
+  if (response.status !== 200) {
+    console.log(`Error with the request! ${response.status}`);
+    return;
+  }
+  return response.json();
+};
+
+export const getData = querystring => {
+  return fetch(
+    `https://api.airtable.com/v0/appnOxIi3Xwhtwq3N/Services%20Database?api_key=${process.env.AIRTABLE_TOKEN}&view=${queryString}`
+  )
+    .then(checkResponse)
+    .then(data => {})
+    .catch(err => {
+      throw new Error(`fetch getData failed ${err}`);
+    });
+};
