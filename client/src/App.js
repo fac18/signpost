41a2b726564075adf84df956d3e79930d9b69160
renatajarmova <renatajarmova@gmail.com>
2020-01-23 11:56:08 +0000

    Add initial about us and landing page components with React Router connections

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Map from "./components/Map/Map";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Map from "./components/Map/Map";
+import LandingPage from "./components/LandingPage/LandingPage";
+import IconsPage from "./components/IconsPage/IconsPage";
+import AboutUs from "./components/AboutUs/AboutUs";
+import AddNewService from "./components/AddNewService/AddNewService";
+import ThankYou from "./components/ThankYou/ThankYou";
 
 function App() {
   return (
-    <div>
-      <Map />
-    </div>
+    <Router>
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/about' component={AboutUs} />
+      <Route path='/iconspage' component={IconsPage} />
+      <Route path='/map' component={Map} />
+      <Route path='/addservice' component={AddNewService} />
+      <Route path='thankyou' component={ThankYou} />
+    </Router>
   );
 }
 
