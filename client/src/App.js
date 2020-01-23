14a27e86675f2f84a818ec08713920734ec74379
renatajarmova <renatajarmova@gmail.com>
2020-01-23 12:39:18 +0000

    Add placeholder for service info component and linked to map

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import AddNewService from "./components/AddNewService/AddNewService";
 import SuggestChange from "./components/SuggestChange/SuggestChange";
 import ThankYou from "./components/ThankYou/ThankYou";
+import Info from "./components/ServiceInformation/ServiceInformation";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path='/addservice' component={AddNewService} />
       <Route path='/suggestchange' component={SuggestChange} />
       <Route path='thankyou' component={ThankYou} />
+      <Route path='/info' component={Info} />
     </Router>
   );
 }
