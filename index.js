0931438f90197d9c195638e9026bd6b422f21245
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-01-23 11:13:47 +0000

    blah

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const path = require("path");
-const generatePassword = require("password-generator");
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get("/api/passwords", (req, res) => {
+app.get('/api/passwords', (req, res) => {
   const count = 5;
 
   // Generate some passwords
@@ -24,8 +24,8 @@ app.get("/api/passwords", (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
