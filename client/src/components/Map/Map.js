334e19ef84a035153637ba7e204fa06ff0f733d2
Gillian <gillian@yeomans.org.uk>
2020-01-23 15:48:26 +0000

    populate markers and setstate of clicked marker
    Co-authored-by: Renata <renatajarmova@gmail.com>

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_TOKEN;
-// const GOOGLE_MAP_API_KEY = "AIzaSyAcnwNgW-XQp7AhqI2kUM_MPhlVGFe4t5E";
 
 const myLocation = {
   lat: 51.5458,
@@ -14,7 +13,32 @@ const mapStyles = {
   height: "400px"
 };
 
-const Map = () => {
+//fake airtable data
+const data = [
+  {
+    Name: "The Margins Project",
+    Lat: "51.5449449",
+    Lng: "-0.1049151"
+  },
+  {
+    Name: "Other Project",
+    Lat: "51.555667",
+    Lng: "-0.0991213"
+  },
+  {
+    Name: "And Another Project",
+    Lat: "51.543596",
+    Lng: "-0.091032"
+  }
+];
+
+//fake state of current service
+const service = "address";
+
+const Map = ({ airTableData }) => {
+  //fake state: selected marker
+  const [selectedMarker, setSelectedMarker] = React.useState(null);
+
   // refs
   const googleMapRef = React.createRef();
   const googleMap = React.useRef(null);
@@ -30,23 +54,42 @@ const Map = () => {
       }
     });
 
-  const createMarker = () =>
-    new window.google.maps.Marker({
-      position: { lat: myLocation.lat, lng: myLocation.lng },
-      map: googleMap.current
-    });
+  function createMarkers() {
+    //change 'data' in here when we have the real data plumbing
+    for (var i = 0; i < data.length; i++) {
+      const marker = new window.google.maps.Marker({
+        position: {
+          lat: Number(data[i].Lat),
+          lng: Number(data[i].Lng)
+        },
+        map: googleMap.current,
+        //change service when we have the real data plumbing
+        icon: `./${service}.png`,
+        animation: window.google.maps.Animation.DROP,
+        title: data[i].Name
+      });
+
+      marker.addListener("click", function(e) {
+        // console.log(e.tb.target.parentNode.title);
+        setSelectedMarker(e.tb.target.parentNode.title);
+      });
+    }
+  }
 
   React.useEffect(() => {
     const googleMapScript = document.createElement("script");
-    console.log("HELLO!", process.env.REACT_APP_GOOGLE_MAPS_TOKEN);
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
     window.document.body.appendChild(googleMapScript);
 
     googleMapScript.addEventListener("load", () => {
       googleMap.current = createGoogleMap();
-      marker.current = createMarker();
+      marker.current = createMarkers();
     });
   }, []);
+
+  React.useEffect(() => {
+    console.log(selectedMarker);
+  }, [selectedMarker]);
 
   return (
     <div>
