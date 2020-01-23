4972a60a446ebeb4d8b1ac10bd41f6a7f8a5e016
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-23 17:48:59 +0000

    Amend order of categories for importance, fix map.svg code, and initialise icon styling

--- client/src/components/IconsPage/IconsPage.js
+++ client/src/components/IconsPage/IconsPage.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React from 'react';
 
-import { ReactComponent as Advice } from "./icons/Advice.svg";
-import { ReactComponent as Animals } from "./icons/Animals.svg";
-import { ReactComponent as Children } from "./icons/Children.svg";
-import { ReactComponent as Food } from "./icons/Food.svg";
-import { ReactComponent as Laundry } from "./icons/Laundry.svg";
-import { ReactComponent as Learning } from "./icons/Learning.svg";
-import { ReactComponent as Map } from "./icons/Map.svg";
-import { ReactComponent as Medical } from "./icons/Medical.svg";
-import { ReactComponent as Shelter } from "./icons/Shelter.svg";
-import { ReactComponent as Shower } from "./icons/Shower.svg";
-import { ReactComponent as Wellbeing } from "./icons/Wellbeing.svg";
-import { ReactComponent as Women } from "./icons/Women.svg";
+// import { ReactComponent as Advice } from './icons/Advice.svg';
+// import { ReactComponent as Animals } from './icons/Animals.svg';
+// import { ReactComponent as Children } from './icons/Children.svg';
+// import { ReactComponent as Food } from './icons/Food.svg';
+// import { ReactComponent as Laundry } from './icons/Laundry.svg';
+// import { ReactComponent as Learning } from './icons/Learning.svg';
+// import { ReactComponent as Map } from './icons/Map.svg';
+// import { ReactComponent as Medical } from './icons/Medical.svg';
+// import { ReactComponent as Shelter } from './icons/Shelter.svg';
+// import { ReactComponent as Shower } from './icons/Shower.svg';
+// import { ReactComponent as Wellbeing } from './icons/Wellbeing.svg';
+// import { ReactComponent as Women } from './icons/Women.svg';
 
 import './IconsPage.css';
 
-const Icons = () => {
+const categories = [
+  'Shelter',
+  'Shower',
+  'Medical',
+  'Food',
+  'Learning',
+  'Women',
+  'Wellbeing',
+  'Advice',
+  'Children',
+  'Animals',
+  'Laundry',
+  'Map'
+];
+
+const Icons = props => {
   return (
-<section className="icon-panel">
-<Advice />
-<Animals />
-<Children />
-<Food />
-<Laundry />
-<Learning />
-<Map />
-<Medical/>
-<Shelter />
-<Shower />
-<Wellbeing />
-<Women />
-</section>
-);
-}
+    <section className='icon-panel'>
+      {categories.map(category => (
+        <button className='icon-panel__button' type='submit' value={category}>
+          <img
+            className='icon-panel__button__image'
+            src={require(`./icons/${category}.svg`)}
+            alt={category}
+          />
+        </button>
+      ))}
+    </section>
+  );
+};
+
 export default Icons;
