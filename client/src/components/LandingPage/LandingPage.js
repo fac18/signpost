2f8920efdc6f34b4567a8c8c30f362c5cbb29575
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-24 14:23:13 +0000

    Add logo to landing page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from '../../assets/signpost-logo.svg';
 
 const LandingPage = () => {
   return (
     <>
-      <Link to='/about'>
+      <Link to='/about' className="about-button">
         <button>?</button>
       </Link>
-      <h1>Signpost</h1>
+      <section className="logo">
+      <Logo />
+      </section>
+      <h1 className="signpost">Signpost</h1>
       <p>
         A community-driven service for those experiencing or at risk of
         homelessness in Islington
