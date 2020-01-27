7cdd8200b90f308e92cd38b5f6b3d8691de5d4b4
renatajarmova <renatajarmova@gmail.com>
2020-01-27 22:27:19 +0000

    Add heart svg animation to thank you page

--- client/src/components/ThankYou/ThankYou.js
+++ client/src/components/ThankYou/ThankYou.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ThankYou.css";
 
 // Need to add icons to this page
 
 const ThankYou = () => {
   return (
     <>
-      <h3>Thank you for your contribution!</h3>
+      <h2>Thank you for your contribution!</h2>
+      <img className='heart' src={require(`./Heart.svg`)} alt={"Heart shape"} />
       <p>Our team will review your information and update the service soon.</p>
       <Link to='/icons-page'>
-        <button>Next</button>
+        <button className='next-button'>Next</button>
       </Link>
     </>
   );
