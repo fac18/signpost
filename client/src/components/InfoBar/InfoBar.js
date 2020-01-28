828e611edfd9a907ccaa0710594165b91409eaaf
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-28 13:15:49 +0000

    Style infobar page

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import "./InfoBar.css"
+
 
 const InfoBar = ({ name, description, address, timings }) => {
   console.log(name);
   return (
     <>
-      <fieldset>
-          <legend>{name}</legend>
-          <p>{description}</p>
-          <p>{address}</p>
-          <p>{timings}</p>
-          <p>See more</p>
-
-      </fieldset>
+      <aside>
+          <h3 className="service-name">{name}</h3>
+          <p className="service-text">{address}</p>
+          <p className="service-text">{timings}</p>
+          <p className="service-text">{description}</p>
+          <p className="service-text">See more</p>
+      </aside>
     </>
   );
 };
--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -129,7 +129,7 @@ const Map = ({
         className="search-bar"
         onChange={event => setSearchLocation(event.target.value)}
       ></input>
-      <button className="map-search" onClick={geocodeSearch}>Submit</button>
+      <button className="map-search" onClick={geocodeSearch}>submit</button>
       <div className="wrapper">
         <div id="google-map" ref={googleMapRef} className="map-area"/>
         <div className="over-map">
