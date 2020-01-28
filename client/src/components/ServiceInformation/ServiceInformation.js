4aee04bef816f4ef9315dd7a791e6a33e4c50be5
renatajarmova <renatajarmova@gmail.com>
2020-01-28 12:12:09 +0000

    Add href to phone, email and website to make this elements clickable and functional

--- client/src/components/ServiceInformation/ServiceInformation.js
+++ client/src/components/ServiceInformation/ServiceInformation.js
@@ -8,20 +8,32 @@ const ServiceInfo = ({ selectedMarkerData }) => {
   console.log(selectedMarkerData);
   return (
     <>
-      <Link to="/map">
-        <button className="close-button">
+      <Link to='/map'>
+        <button className='close-button'>
           <Close />
         </button>
       </Link>
       <aside>
         <a>
           <h3>{selectedMarkerData.fields.Name}</h3>
+          <a href={"tel:" + selectedMarkerData.fields.Phone}>
+            {selectedMarkerData.fields.Phone}
+          </a>
+          <br />
+          <a href={"mailto:" + selectedMarkerData.fields.Email}>
+            {selectedMarkerData.fields.Email}
+          </a>
+          <br />
+          <a href={selectedMarkerData.fields.Website} target='_blank'>
+            {selectedMarkerData.fields.Website}
+          </a>
+          <br />
           <p>{selectedMarkerData.fields.Description}</p>
           <p>{selectedMarkerData.fields.Address}</p>
           <p>{selectedMarkerData.fields.Opening}</p>
         </a>
       </aside>
-      <Link to="/suggest-change">
+      <Link to='/suggest-change'>
         <button>Suggest edit</button>
       </Link>
     </>
