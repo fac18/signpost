a976488d82265036e119c889fa59bb5e562bd906
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-01-28 17:38:57 +0000

    remove separate prettier config for react app

--- client/src/components/HelpPage/HelpPage.js
+++ client/src/components/HelpPage/HelpPage.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { ReactComponent as Close } from "../../assets/close.svg";
-import "./HelpPage.css";
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Close } from '../../assets/close.svg'
+import './HelpPage.css'
 
 const instructions = [
-  "Zoom in",
-  "Zoom out",
-  "Location of service",
-  "Search",
-  "See more information",
-  "You are here"
-];
+  'Zoom in',
+  'Zoom out',
+  'Location of service',
+  'Search',
+  'See more information',
+  'You are here',
+]
 
 // Close needs to return to populated map
 const Help = () => {
-  const [instruction, setInstruction] = React.useState(null);
+  const [instruction, setInstruction] = React.useState(null)
   return (
     <>
       <Link to="/map">
@@ -44,7 +43,7 @@ const Help = () => {
         </article>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Help;
+export default Help
