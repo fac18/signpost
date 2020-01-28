7852aa8e12db02fb078cda7fd19b45f33bf81353
Gillian <gillian@yeomans.org.uk>
2020-01-28 18:03:24 +0000

    build variables for following days for opening times

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -13,25 +13,19 @@ const InfoBar = ({ name, description, address, timings }) => {
     friday: 5,
     saturday: 6,
   }
+
   const todayName = Object.keys(daysLookup).find(
     key => daysLookup[key] === today.getDay()
   )
+
   const currentTime = today.getTime()
 
-  //date object for today's closing times
+  //date object for today's opening times
   const [todayOpeningHour, todayOpeningMinutes] = timings[
     todayName
   ].opening.split(':')
   const todayOpeningTimeStamp = new Date()
   todayOpeningTimeStamp.setHours(todayOpeningHour, todayOpeningMinutes)
-
-  //for services that are closed now - when will they reopen?
-  //NB hardcoded day!!
-  const nextOpeningTimeStamp = new Date()
-  const [nextOpeningHour, nextOpeningMinutes] = timings.wednesday.opening.split(
-    ':'
-  )
-  nextOpeningTimeStamp.setHours(nextOpeningHour, nextOpeningMinutes)
 
   //for services that are open now - when will they close?
   const todayClosingTimeStamp = new Date()
@@ -40,19 +34,55 @@ const InfoBar = ({ name, description, address, timings }) => {
   ].closing.split(':')
   todayClosingTimeStamp.setHours(todayClosingHour, todayClosingMinutes)
 
-  let displayValue
+  //for services that are closed now - when will they reopen?
+  //NB hardcoded day!!
+  let todayPlusOne = today.getDay() + 1 > 6 ? 0 : today.getDay() + 1
+  let todayPlusOneName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusOne
+  )
+  let todayPlusTwo = todayPlusOne + 1 > 6 ? 0 : todayPlusOne + 1
+  let todayPlusTwoName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusTwo
+  )
+  let todayPlusThree = todayPlusTwo + 1 > 6 ? 0 : todayPlusTwo + 1
+  let todayPlusThreeName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusThree
+  )
+  let todayPlusFour = todayPlusThree + 1 > 6 ? 0 : todayPlusThree + 1
+  let todayPlusFourName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusFour
+  )
+  let todayPlusFive = todayPlusFour + 1 > 6 ? 0 : todayPlusFour + 1
+  let todayPlusFiveName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusFive
+  )
+  let todayPlusSix = todayPlusFive + 1 > 6 ? 0 : todayPlusFive + 1
+  let todayPlusSixName = Object.keys(daysLookup).find(
+    key => daysLookup[key] === todayPlusSix
+  )
+
+  console.log(todayPlusSixName)
+
+  // if (today + 1)
+  const nextOpeningTimeStamp = new Date()
+  const [nextOpeningHour, nextOpeningMinutes] = timings.wednesday.opening.split(
+    ':'
+  )
+  nextOpeningTimeStamp.setHours(nextOpeningHour, nextOpeningMinutes)
+
+  let openingInfo
 
   if (timings[todayName].opening === 'closed') {
-    displayValue = `Closed | Opens ???`
+    openingInfo = `Closed | Opens ???`
   } else if (todayOpeningTimeStamp > currentTime) {
     //in this case the service has not yet opened today (but will)
-    displayValue = `Closed | Opens ${timings[todayName].opening}`
+    openingInfo = `Closed | Opens ${timings[todayName].opening}`
   } else if (currentTime > todayClosingTimeStamp) {
     //in this case it was open today but has closed: when will it next be open?
-    displayValue = `Closed | Opens ???`
+    openingInfo = `Closed | Opens ???`
   } else {
     //in this case the service should be open now
-    displayValue = `Open | Closes ${timings[todayName].closing}`
+    openingInfo = `Open | Closes ${timings[todayName].closing}`
   }
 
   return (
@@ -61,7 +91,7 @@ const InfoBar = ({ name, description, address, timings }) => {
         <h3 className="service-name">{name}</h3>
         <p className="service-text">{description}</p>
         <p className="service-text">{address}</p>
-        <p className="service-text">{displayValue}</p>
+        <p className="service-text">{openingInfo}</p>
         <p className="service-text">See more</p>
       </aside>
     </>
