295cdc67b4cea66cc4f5b9c44a643754ef715ad9
Gillian <gillian@yeomans.org.uk>
2020-01-29 11:07:16 +0000

    fix merge mistake in InfoBar with class

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './InfoBar.css'
-import { calcOpeningClosing } from '../../utils/calcOpeningClosing'
+import React from "react"
+import "./InfoBar.css"
+import { calcOpeningClosing } from "../../utils/calcOpeningClosing"
 
 const InfoBar = ({ name, description, address, timings }) => {
   const openingInfo = calcOpeningClosing(timings)
 
   return (
     <>
-      <aside>
-        <h3 className="service-name">{name}</h3>
-        <p className="service-text">{description}</p>
-        <p className="service-text">{address}</p>
-        <p className="service-text">{openingInfo}</p>
-        <p className="service-text">See more</p>
+      <aside className="infobar-style">
+        <h3 className="name">{name}</h3>
+        <p>{description}</p>
+        <p>{address}</p>
+        <p>{openingInfo}</p>
+        <p>See more</p>
       </aside>
     </>
   )
