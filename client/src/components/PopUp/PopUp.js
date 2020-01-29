a17e47cfa6ee63548453b75cbbebc2847fef1aee
renatajarmova <renatajarmova@gmail.com>
2020-01-29 11:03:11 +0000

    Delete unnecessary commented out code from PopUp component

--- client/src/components/PopUp/PopUp.js
+++ client/src/components/PopUp/PopUp.js
@@ -1,29 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ReactComponent as Close } from '../../assets/close.svg'
-import './PopUp.css'
+import React from "react"
+import { Link } from "react-router-dom"
+import { ReactComponent as Close } from "../../assets/close.svg"
+import "./PopUp.css"
 
 const PopUp = ({ popUp, setPopUp }) => {
   const hideModal = () => {
     setPopUp(!setPopUp)
   }
-
-  // let poppy = localStorage.getItem('modal')
-
-  //   if(!poppy){
-  //     function PopUp(){
-  //         $('.home-popup').fadeIn(500);
-  //     }
-
-  //     setTimeout(function(){
-  //         PopUp();
-  //     },1000); // 1000 to load it after 1 second from page load
-
-  //     $('.close-popup-btn').click(function() {
-  //         $('.popup').fadeOut(300);
-  //     });
-  //     localStorage.setItem('myPopup','true');
-  // }
 
   return (
     <div className="modal" id="myModal">
