c3d683294ae998d8c124b77d77dd9fc9582d32bc
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-29 15:25:45 +0000

    Make infobar responsive

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import "./InfoBar.css"
-
+import React from 'react'
+import './InfoBar.css'
 
 const InfoBar = ({ name, description, address, timings }) => {
-  console.log(name);
+  console.log(name)
   return (
     <>
-      <aside className="infobar-style">
-          <h3 className="name">{name}</h3>
-          <p>{address}</p>
-          <p>{timings}</p>
-          <p>{description}</p>
-          <p>See more</p>
-      </aside>
+      <article className="infobar-style">
+        <h3 className="name">{name}</h3>
+        <pre>{address}</pre>
+        <pre>{timings}</pre>
+        <pre>{description}</pre>
+        <pre>See more > </pre>
+      </article>
     </>
-  );
-};
+  )
+}
 
-export default InfoBar;
+export default InfoBar
