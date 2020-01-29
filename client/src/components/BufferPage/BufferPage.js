760dd63a49036d0aee42d526670071ca01cf1e03
renatajarmova <renatajarmova@gmail.com>
2020-01-29 22:21:22 +0000

    Remove previous SVG displaying code from Buffer componenet

--- client/src/components/BufferPage/BufferPage.js
+++ client/src/components/BufferPage/BufferPage.js
@@ -6,31 +6,6 @@ const BufferPage = () => {
   return (
     <div className="buffer">
       <MagnifyingGlass />
-      {/* <img
-        src={require(`./magnifying/magnifying-glass.svg`)}
-        alt={'magnifying glass'}
-        className="magnifying-glass"
-      ></img> */}
-      {/* <img
-        src={require(`./icons/food-water-button.svg`)}
-        alt={'food icon'}
-        className="food slide slide1"
-      ></img>
-      <img
-        src={require(`./icons/shelter-button.svg`)}
-        alt={'shelter icon'}
-        className="shelter slide slide2"
-      ></img> */}
-      {/* <img
-        src={require(`./icons/shower-button.svg`)}
-        alt={'shower icon'}
-        className="shower slide slide3"
-      ></img>
-      <img
-        src={require(`./icons/learning-button.svg`)}
-        alt={'learning icon'}
-        className="learning slide slide4"
-      ></img> */}
       <p>Finding community contributions...</p>
     </div>
   )
