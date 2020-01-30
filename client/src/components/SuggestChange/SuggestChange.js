e797b0bdf6d24c850e893991f138c865b0feda45
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-30 15:21:38 +0000

    Style suggest change page

--- client/src/components/SuggestChange/SuggestChange.js
+++ client/src/components/SuggestChange/SuggestChange.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Close } from '../Buttons/buttons.js'
+import './SuggestChange.css'
 
 // Do something with the text area inputed by the user -> send it somewhere?
 
@@ -17,23 +18,28 @@ const SuggestChange = () => {
       <Link to="/map">
         <Close />
       </Link>
-      <h1>Suggest a change</h1>
-      <p>
-        If you see something wrong about this service, please suggest a change
-        and we'll update it for you!
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          rows="10"
-          cols="30"
-          value={textInput}
-          onChange={event => setTextInput(event.target.value)}
-        ></textarea>
-        <br />
-        <Link to="/thank-you">
-          <button type="submit">Post</button>
-        </Link>
-      </form>
+      <fieldset className="change-text">
+        <legend>Suggest a change</legend>
+        <p>
+          If you see something wrong about this service, please suggest a change
+          and we'll update it for you!
+        </p>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            className="suggestion-box"
+            rows="10"
+            cols="30"
+            value={textInput}
+            onChange={event => setTextInput(event.target.value)}
+          ></textarea>
+          <br />
+          <Link to="/thank-you">
+            <button className="change-button" type="submit">
+              Post
+            </button>
+          </Link>
+        </form>
+      </fieldset>
     </>
   )
 }
