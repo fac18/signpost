aa58e5d04962bbe7d8e0f55df7bc471605ea61fc
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-30 15:47:59 +0000

    Make suggest change page responsive and adjust details

--- client/src/components/Buttons/buttons.js
+++ client/src/components/Buttons/buttons.js
@@ -11,29 +11,60 @@ const Add = styled(AddSvg)`
   position: fixed;
   right: 1vw;
   top: 1vw;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: fixed;
+    width: 10vw;
+    right: 1vw;
+    top: 1vw;
+
 `
 
 const About = styled(AboutSvg)`
   position: fixed;
   left: 1vw;
   top: 1vw;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: fixed;
+    width: 10vw;
+    right: 1vw;
+    top: 1vw;
 `
 
 const Close = styled(CloseSvg)`
   position: fixed;
   left: 0.5vw;
   top: 0.5vw;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: fixed;
+    width: 12vw;
+    left: 0.5vw;
+    top: 0.5vw;
+
 `
 
 const ClosePopup = styled(ClosePopupSvg)`
   position: relative;
   float: left;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: relative;
+    width: 10vw;
+    float: left;
 `
 
 const Help = styled(HelpSvg)`
   position: fixed;
   right: 1vw;
-  top: 1vw;
+  top: 1.25vw;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: fixed;
+    width: 9vw;
+    right: 1vw;
+    top: 1.5vw;
 `
 
 // USE REN'S ONE
@@ -47,6 +78,12 @@ const Back = styled(BackSvg)`
   position: fixed;
   left: 1vw;
   top: 1vw;
+
+  @media screen and (min-width: 320px) and (max-width: 640px) {
+    position: fixed;
+    width: 5vw;
+    left: 1.5vw;
+    top: 1vw;
 `
 
 export { Add, About, Close, Help, Back, ClosePopup }
