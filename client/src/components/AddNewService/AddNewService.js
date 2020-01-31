b907a270ec263735780bc27dea2fb76354fe5778
renatajarmova <renatajarmova@gmail.com>
2020-01-31 10:21:15 +0000

    Redirect form on submit after post method to thank you page
    Closes #35

--- client/src/components/AddNewService/AddNewService.js
+++ client/src/components/AddNewService/AddNewService.js
@@ -1,8 +1,8 @@
-import React, { useReducer } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useReducer } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import { Close } from '../Buttons/buttons.js'
-import postAddServiceData from '../../utils/postData';
-import './AddNewService.css';
+import postAddServiceData from '../../utils/postData'
+import './AddNewService.css'
 
 const initialState = {
   Title: '',
@@ -11,21 +11,22 @@ const initialState = {
   Service: 'select-a-service',
   Contact_Name: '',
   Contact_Details: '',
-};
+}
 
 function reducer(state, { field, value }) {
   return {
     ...state,
     [field]: value,
-  };
+  }
 }
 
 const AddNewService = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const [changePage, setChangePage] = React.useState(false)
 
   const onChange = e => {
-    dispatch({ field: e.target.name, value: e.target.value });
-  };
+    dispatch({ field: e.target.name, value: e.target.value })
+  }
 
   const {
     Title,
@@ -34,15 +35,15 @@ const AddNewService = () => {
     Service,
     Contact_Name,
     Contact_Details,
-  } = state;
-
+  } = state
 
   const handleSubmit = event => {
-    event.preventDefault();
-    postAddServiceData(state);
-    console.log({ state });
-    console.log(event);
-  };
+    event.preventDefault()
+    postAddServiceData(state)
+    setChangePage(!changePage)
+    console.log({ state })
+    console.log(event)
+  }
 
   return (
     <>
@@ -51,129 +52,137 @@ const AddNewService = () => {
       </Link>
       <fieldset className="add-form">
         <legend>Add a new service</legend>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="Title">
-            <input
-              type="text"
-              name="Title"
-              id="Title"
-              className="form__input"
-              placeholder="Title"
-              value={Title}
-              onChange={onChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="Location">
+        {changePage ? (
+          <Redirect to="thank-you"></Redirect>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="Title">
+              <input
+                type="text"
+                name="Title"
+                id="Title"
+                className="form__input"
+                placeholder="Title"
+                value={Title}
+                onChange={onChange}
+              />
+            </label>
             <br />
-            <input
-              type="text"
-              name="Location"
-              id="Location"
-              className="form__input"
-              placeholder="Location"
-              value={Location}
-              onChange={onChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="Opening_Times">
+            <label htmlFor="Location">
+              <br />
+              <input
+                type="text"
+                name="Location"
+                id="Location"
+                className="form__input"
+                placeholder="Location"
+                value={Location}
+                onChange={onChange}
+              />
+            </label>
             <br />
-            <input
-              type="text"
-              name="Opening_Times"
-              id="Opening_Times"
-              className="form__input"
-              placeholder="Opening times"
-              value={Opening_Times}
-              onChange={onChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="service-input">
+            <label htmlFor="Opening_Times">
+              <br />
+              <input
+                type="text"
+                name="Opening_Times"
+                id="Opening_Times"
+                className="form__input"
+                placeholder="Opening times"
+                value={Opening_Times}
+                onChange={onChange}
+              />
+            </label>
             <br />
-            <select
-              selected={Service}
-              aria-label="select services dropdown"
-              type="dropdown"
-              name="Service"
-              id="Service"
-              className="dropdown"
-              required
-              value={Service}
-              onChange={onChange}
+            <label htmlFor="service-input">
+              <br />
+              <select
+                selected={Service}
+                aria-label="select services dropdown"
+                type="dropdown"
+                name="Service"
+                id="Service"
+                className="dropdown"
+                required
+                value={Service}
+                onChange={onChange}
+              >
+                <option value="select-a-service" disabled>
+                  Select a service
+                </option>
+                <option name="Service" value="Advice">
+                  Advice
+                </option>
+                <option name="Service" value="Animals">
+                  Animals
+                </option>
+                <option name="Service" value="Children">
+                  Children
+                </option>
+                <option name="Service" value="Clothing">
+                  Clothing
+                </option>
+                <option name="Service" value="Food">
+                  Food
+                </option>
+                <option name="Service" value="Learning">
+                  Learning
+                </option>
+                <option name="Service" value="Medical">
+                  Medical
+                </option>
+                <option name="Service" value="Shower">
+                  Shower
+                </option>
+                <option name="Service" value="Shelter">
+                  Shelter
+                </option>
+                <option name="Service" value="Wellbeing">
+                  Wellbeing
+                </option>
+                <option name="Service" value="Women">
+                  Women
+                </option>
+              </select>
+            </label>
+            <br />
+            <label htmlFor="Contact_Name">
+              <br />
+              <input
+                type="text"
+                name="Contact_Name"
+                id="Contact_Name"
+                className="form__input"
+                placeholder="Contact name"
+                value={Contact_Name}
+                onChange={onChange}
+              />
+            </label>
+            <br />
+            <label htmlFor="Contact_Details">
+              <br />
+              <input
+                type="text"
+                name="Contact_Details"
+                id="Contact_Details"
+                className="form__input"
+                placeholder="Contact details"
+                value={Contact_Details}
+                onChange={onChange}
+              />
+            </label>
+            <br />
+            <button
+              type="submit"
+              className="submit-button"
+              data-testid="add-button"
             >
-              <option value="select-a-service" disabled>
-                Select a service
-              </option>
-              <option name="Service" value="Advice">
-                Advice
-              </option>
-              <option name="Service" value="Animals">
-                Animals
-              </option>
-              <option name="Service" value="Children">
-                Children
-              </option>
-              <option name="Service" value="Clothing">
-                Clothing
-              </option>
-              <option name="Service" value="Food">
-                Food
-              </option>
-              <option name="Service" value="Learning">
-                Learning
-              </option>
-              <option name="Service" value="Medical">
-                Medical
-              </option>
-              <option name="Service" value="Shower">
-                Shower
-              </option>
-              <option name="Service" value="Shelter">
-                Shelter
-              </option>
-              <option name="Service" value="Wellbeing">
-                Wellbeing
-              </option>
-              <option name="Service" value="Women">
-                Women
-              </option>
-            </select>
-          </label>
-          <br />
-          <label htmlFor="Contact_Name">
-            <br />
-            <input
-              type="text"
-              name="Contact_Name"
-              id="Contact_Name"
-              className="form__input"
-              placeholder="Contact name"
-              value={Contact_Name}
-              onChange={onChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="Contact_Details">
-            <br />
-            <input
-              type="text"
-              name="Contact_Details"
-              id="Contact_Details"
-              className="form__input"
-              placeholder="Contact details"
-              value={Contact_Details}
-              onChange={onChange}
-            />
-          </label>
-          <br />
-          <button type="submit" className="submit-button" data-testid="add-button">
-            Add
-          </button>
-        </form>
+              Add
+            </button>
+          </form>
+        )}
       </fieldset>
     </>
-  );
-};
-export default AddNewService;
+  )
+}
+export default AddNewService
--- client/src/components/SuggestChange/SuggestChange.js
+++ client/src/components/SuggestChange/SuggestChange.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { Close } from '../Buttons/buttons.js'
 import postEditData from '../../utils/postEditData'
 import './SuggestChange.css'
@@ -17,6 +17,7 @@ function reducer(state, { field, value }) {
 
 const SuggestChange = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
+  const [changePage, setChangePage] = React.useState(false)
 
   const onChange = e => {
     dispatch({ field: e.target.name, value: e.target.value })
@@ -27,20 +28,10 @@ const SuggestChange = () => {
   const handleSubmit = event => {
     event.preventDefault()
     postEditData(state)
+    setChangePage(!changePage)
     console.log({ state })
     console.log(event)
   }
-
-  // const  = () => {
-  //   const [textInput, setTextInput] = React.useState('')
-
-  //   const handleSubmit = event => {
-  //     //write call to backend to insert data (from state)
-  //     event.preventDefault()
-  //     postEditData(textInput)
-  //     console.log({ textInput })
-  //     console.log(event)
-  //   }
 
   return (
     <>
@@ -53,21 +44,25 @@ const SuggestChange = () => {
           If you see something wrong about this service, please suggest a change
           and we'll update it for you!
         </p>
-        <form onSubmit={handleSubmit}>
-          <textarea
-            name="textInput"
-            type="textarea"
-            className="suggestion-box"
-            rows="10"
-            cols="30"
-            value={textInput}
-            onChange={onChange}
-          ></textarea>
-          <br />
-          <button className="change-button" type="submit">
-            Post
-          </button>
-        </form>
+        {changePage ? (
+          <Redirect to="thank-you"></Redirect>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <textarea
+              name="textInput"
+              type="textarea"
+              className="suggestion-box"
+              rows="10"
+              cols="30"
+              value={textInput}
+              onChange={onChange}
+            ></textarea>
+            <br />
+            <button className="change-button" type="submit">
+              Post
+            </button>
+          </form>
+        )}
       </fieldset>
     </>
   )
